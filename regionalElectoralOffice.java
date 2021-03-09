ca3b0ddfeb6c963dc0a6e6ca0223862d07dff1b3
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-03-09 22:58:08 +0000

    Update regionalElectoralOffice.java

--- regionalElectoralOffice.java
+++ regionalElectoralOffice.java
@@ -1,12 +1,45 @@
+
+
+/**
+ * @author Michael Ofori(Group 5)
+ * Regional Electoral Office Class.
+ */
+
 public class regionalElectoralOffice {
     private int regionalElectoralOfficeID;
-
+    private String regionName;
+    
+    /**
+     * @return regionalElectoralOfficeID
+     */
     public int getRegionalElectoralOfficeID() {
         return regionalElectoralOfficeID;
     }
-
+    /**
+     * @return regionName
+     */
+    public String getRegionName() {
+        return regionName;
+    }
+    /**
+     * @param regionName
+     */
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+    /**
+     * @param regionalElectoralOfficeID
+     */
     public void setRegionalElectoralOfficeID(int regionalElectoralOfficeID) {
         this.regionalElectoralOfficeID = regionalElectoralOfficeID;
+    }
+    /**
+     * @param regionalElectoralOfficeID
+     * @param regionName
+     */
+    public regionalElectoralOffice(int regionalElectoralOfficeID, String regionName) {
+        this.regionalElectoralOfficeID = regionalElectoralOfficeID;
+        this.regionName = regionName;
     }
     
 }
