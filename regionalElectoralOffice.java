7fbc5d78041d135cae35192f5bd95aaa1f7d11dd
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-03-10 22:23:47 +0000

    Update regionalElectoralOffice.java

--- regionalElectoralOffice.java
+++ regionalElectoralOffice.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 /**
  * @author Michael Ofori(Group 5)
@@ -8,6 +8,10 @@
 public class regionalElectoralOffice {
     private int regionalElectoralOfficeID;
     private String regionName;
+
+
+    // Hashmap Regional Level 
+    HashMap<DistrictElectoralOffice, Integer> votes = new HashMap<DistrictElectoralOffice, Integer>();
     
     /**
      * @return regionalElectoralOfficeID
@@ -43,14 +47,51 @@ public class regionalElectoralOffice {
     }
     /**
      * Count method to count the number of votes in the region.
-     * @param results
-     * @return
+     * @param regionn
+     * @return votes
      */
-    public static int regionCount(regionalElectoralOffice results){
-        int count=0;
-        count++;
-        return count;
+    public void regionalLevel(DistrictElectoralOffice region) {
+        if (votes.containsKey(region)) {
+            int curVotes = votes.get(region);
+            votes.replace(region, curVotes+1);
+        } else {
+            votes.put(region, 1);
+        }
     }
+
+    public void Counter(){
+        System.out.println(votes);
+    }
+
+    public void maxCount() {
+        int maxValue = (Collections.max(votes.values()));
+
+        int maxValueInMap=(Collections.max(votes.values()));  // This will return max value in the Hashmap
+        DistrictElectoralOffice winner = new DistrictElectoralOffice(maxValueInMap, regionName);
+        for (Map.Entry<DistrictElectoralOffice, Integer> entry : votes.entrySet()) {  // Itrate through hashmap
+            if (entry.getValue()==maxValueInMap) {
+                winner = entry.getKey();     // Print the key with max value
+            }
+        }
+
+        System.out.println("Winner: " + winner.getdistrictName() + ":" + votes.get(winner) + " votes");
+
+    }
+    public void minCount() {
+        int maxValue = (Collections.max(votes.values()));
+
+        int maxValueInMap=(Collections.min(votes.values()));  // This will return max value in the Hashmap
+        DistrictElectoralOffice winner = new DistrictElectoralOffice(maxValueInMap, regionName);
+        for (Map.Entry<DistrictElectoralOffice, Integer> entry : votes.entrySet()) {  // Itrate through hashmap
+            if (entry.getValue()==maxValueInMap) {
+                winner = entry.getKey();     // Print the key with max value
+            }
+        }
+
+        System.out.println("Last place: " + winner.getdistrictName() + ":" + votes.get(winner) + " votes");
+
+    }
+
     /**
      * To string method
      * @return the regionID and name.
