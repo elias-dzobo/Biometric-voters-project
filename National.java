667cba6b9a30b68cd5201fb10660d314e2b909a4
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-03-11 08:53:18 +0000

    Update National.java

--- National.java
+++ National.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
-public class National {
-    public String NationName;
+public class National  {
+    String NationName;
+
     HashMap<regionalElectoralOffice, Integer> votes = new HashMap<regionalElectoralOffice, Integer> ();
 
         National (String NationName){
@@ -12,33 +13,42 @@ public class National {
         public String getNationName(){
             return NationName;
         }
+// Getter and Setter methods
 
+public String getNationName(){
+    return this.NationName;
+}
+
+public void setNationName(String NationName) {
+    this.NationName = NationName;
+}
 
     public void maxCount() {
         int maxValue = (Collections.max(votes.values()));
 
         int maxValueInMap=(Collections.max(votes.values()));  // This will return max value in the Hashmap
-        regionalElectoralOffice winner = new regionalElectoralOffice(maxValueInMap, regionName);
+        regionalElectoralOffice winner = new regionalElectoralOffice();
         for (Map.Entry<regionalElectoralOffice, Integer> entry : votes.entrySet()) {  // Itrate through hashmap
             if (entry.getValue()==maxValueInMap) {
                 winner = entry.getKey();     // Print the key with max value
             }
         }
-        System.out.println("Winner: " + winner.getregionName() + ":" + votes.get(winner) + " votes");
+        System.out.println("Winner: " + winner.ge + ":" + votes.get(winner) + " votes");
+
     }
 
         public void minCount() {
             int minValue = (Collections.max(votes.values()));
     
             int minValueInMap=(Collections.min(votes.values()));  // This will return max value in the Hashmap
-            regionalElectoralOffice last = new regionalElectoralOffice(minValueInMap, regionName);
+            regionalElectoralOffice last = new regionalElectoralOffice();
             for (Map.Entry<regionalElectoralOffice, Integer> entry : votes.entrySet()) {  // Itrate through hashmap
                 if (entry.getValue()==minValueInMap) {
                     last = entry.getKey();     // Print the key with max value
                 }
             }
     
-            System.out.println("Last place: " + last.getregionName() + ":" + votes.get(last) + " votes");
+            System.out.println("Last place: " + last.getRegionName" votes");
     
         }
 
